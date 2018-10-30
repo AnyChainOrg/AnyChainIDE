f189e3f66eecd893d2b95f47b8c8aefa039a792e
thomas <47029316@qq.com>
2018-10-30 16:47:16 +0800

    debug

--- ChainIDE/mainwindow.cpp
+++ ChainIDE/mainwindow.cpp
@@ -83,6 +83,7 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     qDebug()<<"delete mainwindow";
+    qInstallMessageHandler(0);
     delete _p;
     _p = nullptr;
     delete ui;
